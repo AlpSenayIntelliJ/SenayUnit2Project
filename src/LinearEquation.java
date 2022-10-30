4a912e33b7f031eff190718f3ab638b3610b844e
Alp Senay <alps2789@nycstudents.net>
2022-10-30 19:23:21 -0400

    working...

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1, x2, y1, y2;
+    public void lineInfo() {
+        int slopen = y2 - y1;
+        int sloped = x2 - x1;
+        double slope = (double) slopen/sloped;
+        double yint = y1 - ((slope)*x1);
+        double distance = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        System.out.println("The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")");
+        System.out.println("The equation of the line between these points is: y = " + slopen + "/" + sloped
+        + "x + " + yint);
+        System.out.println("The slope of this line is: " + slope);
+        System.out.println("The y-intercept of this line is: " + yint);
+        System.out.println("The distance between the two points is: " + distance);
+
+
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Welcome!\nEnter coordinate 1: ");
+        String coord1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        String coord2 = scan.nextLine();
+
+        int coord1x = Integer.parseInt(coord1.substring(1, coord1.indexOf(",")));
+        int coord1y = Integer.parseInt(coord1.substring(coord1.indexOf(", ")+2,coord1.indexOf(")")));
+        int coord2x = Integer.parseInt(coord2.substring(1, coord2.indexOf(",")));
+        int coord2y = Integer.parseInt(coord2.substring(coord2.indexOf(", ")+2,coord2.indexOf(")")));
+
+
+    }
 }
